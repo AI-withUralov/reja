cabdf8422184ab7ca9f2ec351825d862ebf6c8d9
Sherzodbek <sherzoduralov285@gmail.com>
2024-08-16 14:48:16 +0900

    feat: develop author ejs router

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web Serverni boshlash");
 const express = require('express');
 const app = express();
 const http = require("http");
+const fs = require('fs');
+
+let user;
+fs.readFile('database/user.json', 'utf8', (err, data) => {
+    if(err) {
+        console.log('Error:', err);
+    }else {
+        user = JSON.parse(data)
+    }
+});
 
 //1 Kirish code
 app.use(express.static('public'));  /// har qanday browserdan kirib kelgan zaproslar uchun public folder ochiq
@@ -23,9 +33,13 @@ app.post("/create-item", (req, res) => {    // malumotni olib keladi va database
     res.json({test: "success"});
 })
 
-app.get('/', function (req, res) {   /// databasedagi ma'lumotni uqsh uchun get
-    res.render('harid');
+app.get('/author', (req, res) => {
+    res.render('author',  {user: user});
 });
+
+// app.get('/', function (req, res) {   /// databasedagi ma'lumotni uqsh uchun get
+//     res.render('harid');
+// });
 
 
 const server = http.createServer(app);
